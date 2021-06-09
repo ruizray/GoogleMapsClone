bca5cd80eab6b689a2287c9ace64ff292c83b1ed
Ray (Home) <raymundoruiz91@gmail.com>
2021-06-09 01:13:03 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Login from "./Authentication/Login";
+import { useState } from "react";
+import Register from "./Authentication/Register";
+import { Route, NavLink, Router, Switch, Redirect } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Route exact path='/'>
+				<Redirect to='/login' />
+			</Route>
+			<Route exact path='/login' component={Login} />
+			<Route exact path='/register' component={Register} />
+		</>
+	);
+	// if (register === true) {
+	// 	return <Register setRegister={setRegister}></Register>;
+	// } else {
+	// 	return <Login setRegister={setRegister}></Login>;
+	// }
 }
 
 export default App;
---
+++ src/Authentication/Login.jsx
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Joi from "joi-browser";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { NavLink } from "react-router-dom";
+
+function Copyright() {
+	return (
+		<Typography variant='body2' color='textSecondary' align='center'>
+			{"Copyright © "}
+			<Link color='inherit' href='https://material-ui.com/'>
+				Your Website
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
+}
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(8),
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: "100%", // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+}));
+
+const Login = props => {
+	const schema = {
+		email: Joi.string().required().label("Email"),
+		password: Joi.string().min(6).required().label("Password"),
+	};
+
+	const [data, setData] = useState({});
+	const [formErrors, setFormErrors] = useState({});
+
+	const validate = () => {
+		const options = { abortEarly: false, allowUnknown: true };
+		const { error } = Joi.validate(data, schema, options);
+		if (!error) return null;
+		const errors = {};
+		for (let item of error.details) errors[item.path[0]] = item.message;
+
+		console.log(errors);
+		return errors;
+	};
+	const validateProperty = ({ name, value }) => {
+		const obj = { [name]: value };
+		const tempschema = { [name]: schema[name] };
+		const { error } = Joi.validate(obj, tempschema);
+		return error ? error.details[0].message : null;
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(e);
+		const errors = validate();
+		console.log(errors);
+		console.log(formErrors);
+		setFormErrors(errors || {});
+
+		if (errors) return;
+		console.log("Here");
+	};
+
+	const handleChange = ({ currentTarget: input }) => {
+		const errors = { ...formErrors };
+		const errorMessage = validateProperty(input);
+		if (errorMessage) {
+			errors[input.name] = errorMessage;
+		} else {
+			delete errors[input.name];
+		}
+
+		const tempdata = { ...data };
+		tempdata[input.name] = input.value;
+		setData(tempdata);
+		setFormErrors(errors);
+	};
+
+	const classes = useStyles();
+	const disabled = validate()
+	return (
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}></Avatar>
+				<Typography component='h1' variant='h5'>
+					Sign in
+				</Typography>
+				<form className={classes.form}>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						onChange={handleChange}
+						id='email'
+						label='Email Address'
+						name='email'
+						autoComplete='email'
+						error={formErrors["email"]}
+						helperText={formErrors["email"]}
+						autoFocus
+					/>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						onChange={handleChange}
+						name='password'
+						label='Password'
+						type='password'
+						id='password'
+						autoComplete='current-password'
+						error={formErrors["password"]}
+						helperText={formErrors["password"]}
+					/>
+
+					<Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit} disabled={disabled}>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item xs>
+							<Link href='#' variant='body2'>
+								Forgot password?
+							</Link>
+						</Grid>
+						<Grid item>
+						
+						<NavLink className='nav-link' to='/register'>
+								"Don't have an account? Sign Up"
+						</NavLink>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+			<Box mt={8}>
+				<Copyright />
+			</Box>
+		</Container>
+	);
+}
+export default Login;
